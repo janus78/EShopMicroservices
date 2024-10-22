bceeeaf937ee97c1f3a690309e53cdacd1486e74
Omar Macias <maciaseo@gmail.com>
2024-10-22 17:02:50 -0600

    Completed initial post endpoint
    Added initial minimal api post e dpoint and configure carter and mediatr in program cs

--- src/Services/Catalog/Catalog.Api/Product/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.Api/Product/CreateProduct/CreateProductEndpoint.cs
@@ -1,5 +1,3 @@
-using Carter;
-
 namespace Catalog.Api.Product.CreateProduct;
 
 public record CreateProductRequest(
@@ -14,6 +12,21 @@ public class CreateProductEndpoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        
+        app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<CreateProductCommand>();
+
+                var result = await sender.Send(command);
+
+                var response = result.Adapt<CreateProductResponse>();
+
+                return Results.Created($"/products/{response.Id}", response);
+            })
+            .WithName("CreateProduct")
+            .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status500InternalServerError)
+            .WithSummary("Create Product")
+            .WithDescription("Create Product");
     }
 }
